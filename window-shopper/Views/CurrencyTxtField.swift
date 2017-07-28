de31b785ba2f96b380bf906b2e681ee6157b260c
Villefrance <emilkoch@gmail.com>
2017-07-28 20:14:14 +0200

    Interface built + custom button

---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Emil Villefrance on 28/07/2017.
+//  Copyright © 2017 Emil Villefrance. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTxtField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
